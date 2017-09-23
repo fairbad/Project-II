a166de31a872dcd3f8a92cd8d81da2eaca12ee15
Andy Zheng <AndyZhengJinQing@gmail.com>
2017-09-23 13:15:27 -0400

    Andy edit spring configuration xml and add template folder

--- DungeonCrawlers/src/main/java/com/dungeoncrawlers/controllers/UserController.java
+++ DungeonCrawlers/src/main/java/com/dungeoncrawlers/controllers/UserController.java
@@ -9,35 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dungeoncrawlers.dto.UserDTO;
 import com.dungeoncrawlers.service.ServiceInterface;
 
-//@RestController
-//@RequestMapping(value="/user")
+@RestController
+@RequestMapping(value="/user")
 public class UserController {
 
-	//@Autowired
-	private ServiceInterface serviceImpl;
+	@Autowired
+	private ServiceInterface serviceimpl;
 
 	public void setServiceImpl(ServiceInterface serviceImpl) {
-		this.serviceImpl = serviceImpl;
-	}
-/*  //TODO: Needs a dto
-	@RequestMapping(value="/auth", method= {RequestMethod.POST},
-			consumes= {MediaType.APPLICATION_JSON_VALUE},
-			produces= {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<UserDto> 
-		authenticateUser(@RequestBody UserDto userDto){	
-		System.out.println("authenticating user");
-		return new ResponseEntity<UserDto>(serviceImpl.authenticateUser(userDto),HttpStatus.OK);
+		this.serviceimpl = serviceImpl;
 	}
 
 	@RequestMapping(value="/register", method= {RequestMethod.POST},
 			consumes= {MediaType.APPLICATION_JSON_VALUE},
 			produces= {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<UserDto>	registerUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDTO>	registerUser(@RequestBody UserDTO userDTO){
 		System.out.println("creating new user");
-		serviceImpl.createUser(userDto);
-		return new ResponseEntity<UserDto>(userDto, HttpStatus.OK);
+		serviceimpl.add(userDTO);
+		return new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
 	}
-*/
 }
